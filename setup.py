9cb195b6048bfe397dda5d4d00f748e878749f16
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2024-01-25 14:32:28 +0000

    Fix issues with the setup config

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ Jinja templates for Digital Marketplace apps.
 """
 import re
 import ast
+import glob
+import os
 from setuptools import setup, find_packages
 
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('digitalmarketplace_frontend_jinja/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+    version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
+
+components = []
+directories = glob.glob("digitalmarketplace_frontend_jinja/**/**/*.html", recursive=True)
+for directory in directories:
+    components.append(os.path.relpath(os.path.dirname(directory), "digitalmarketplace_frontend_jinja") + "/*.html")
 
 setup(
     name='ccs-digitalmarketplace-frontend-jinja',
@@ -21,7 +27,7 @@ setup(
     description='Jinja templates for Digital Marketplace apps.',
     long_description=__doc__,
     packages=find_packages(),
-    package_data={'digitalmarketplace_frontend_jinja': ['py.typed']},
+    package_data={'digitalmarketplace_frontend_jinja': components},
     include_package_data=True,
     install_requires=[
         'jinja2<3',
